704c1dcaae39f1bdd2263eacdaf031f867579228
ViktorUdovin <viktorudovinwork@gmail.com>
2020-06-20 14:32:03 +0400

    update lombok

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,10 +1,8 @@
 package ru.netology.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Radio {

cf61de74e22054803af883b1a912ef06c1c5562a
ViktorUdovin <viktorudovinwork@gmail.com>
2020-06-20 14:30:33 +0400

    update code in radio and radiotest

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -4,74 +4,71 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Radio {
     private String name;
-    private int maxVolume;
-    private int minVolume;
+    private int maxVolume = 100;
+    private int minVolume = 0;
     private int currentVolume;
     private int currentStation;
-    private int firstStation;
-    private int lastStation;
-    private int editCurrentStation;
+    private int firstStation = 1;
+    private int lastStation = 10;
+
+
+    public Radio(String name, int currentVolume, int currentStation, boolean on) {
+        this.name = name;
+        this.currentVolume = currentVolume;
+        this.currentStation = currentStation;
+        this.on = on;
+    }
+
+
+
     private boolean on = true;
 
 
 
 
     public void increaseCurrentVolume() {
-        if (currentVolume >= maxVolume) {
-            System.out.println("Максимальная громкость");
+        if (currentVolume == maxVolume) {
             return;
         }
-        setCurrentVolume(this.currentVolume + 1);
+        currentVolume++;
     }
 
     public void decreaseCurrentVolume() {
-        if (currentVolume <= minVolume) {
-            System.out.println("Минимальная громкость");
+        if (currentVolume == minVolume) {
             return;
         }
-        setCurrentVolume(this.currentVolume - 1);
+        currentVolume--;
     }
 
     public void nextStation() {
         if (currentStation == lastStation) {
             currentStation = firstStation;
-            System.out.println("Включена станция #" + currentStation);
             return;
         }
-        setCurrentStation(this.currentStation + 1);
-        System.out.println("Включена станция #" + currentStation);
+        currentStation++;
     }
 
     public void previosStation() {
         if (currentStation == firstStation) {
             currentStation = lastStation;
-            System.out.println("Включена станция #" + currentStation);
             return;
         }
-        setCurrentStation(this.currentStation - 1);
-        System.out.println("Включена станция #" + currentStation);
+        currentStation--;
     }
 
-    public void editcurrentstation() {
-        if (editCurrentStation < firstStation) {
-            setCurrentStation(this.currentStation);
-            System.out.println("Невозможно выбрать станцию #" + editCurrentStation);
-            System.out.println("Включена станция #" + currentStation);
+    public void editcurrentstation(int newStation) {
+        if (newStation < firstStation) {
             return;
         }
-        if (editCurrentStation > lastStation) {
-            setCurrentStation(this.currentStation);
-            System.out.println("Невозможно выбрать станцию #" + editCurrentStation);
-            System.out.println("Включена станция #" + currentStation);
+        if (newStation > lastStation) {
             return;
         }
-        currentStation = editCurrentStation;
-        System.out.println("Включена станция #" + currentStation);
+        currentStation = newStation;
     }
 
 }
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -8,21 +8,21 @@ class RadioTest {
 
     @Test
     public void incraseCurrentVolume() {
-        Radio radio = new Radio("name",100,0,25,5,1,10,5, true);
+        Radio radio = new Radio("name",25,5, true);
         radio.increaseCurrentVolume();
         assertEquals(26, radio.getCurrentVolume());
     }
 
     @Test
     public void decraseCurrentVolume() {
-        Radio radio = new Radio("name",100,0,25,5,1,10, 5,true);
+        Radio radio = new Radio("name",25,5, true);
         radio.decreaseCurrentVolume();
         assertEquals(24, radio.getCurrentVolume());
     }
 
     @Test
     public void incraseCurrentVolumeLimit() {
-        Radio radio = new Radio("name",100,0,100,5,1,10, 5,true);
+        Radio radio = new Radio("name",100,5, true);
         radio.increaseCurrentVolume();
         assertEquals(100, radio.getCurrentVolume());
     }
@@ -30,55 +30,55 @@ class RadioTest {
 
     @Test
     public void decraseCurrentVolumeLimit() {
-        Radio radio = new Radio("name",100,0,0,5,1,10, 5,true);
+        Radio radio = new Radio("name",0,5, true);
         radio.decreaseCurrentVolume();
         assertEquals(0, radio.getCurrentVolume());
     }
 
     @Test
     public void previosStationFirst() {
-            Radio radio = new Radio("name",100,0,100,1,1,10, 1,true);
+            Radio radio = new Radio("name",100,1, true);
             radio.previosStation();
             assertEquals(10, radio.getCurrentStation());
         }
 
     @Test
     public void nextStationLast() {
-        Radio radio = new Radio("name",100,0,100,10,1,10, 10,true);
+        Radio radio = new Radio("name",100,10, true);
         radio.nextStation();
         assertEquals(1, radio.getCurrentStation());
     }
 
     @Test
     public void nextstation() {
-        Radio radio = new Radio("name",100,0,100,5,1,10, 5,true);
+        Radio radio = new Radio("name",100,5, true);
         radio.nextStation();
         assertEquals(6, radio.getCurrentStation());    }
 
     @Test
     public void previosStation() {
-        Radio radio = new Radio("name",100,0,100,5,1,10, 5,true);
+        Radio radio = new Radio("name",100,5, true);
         radio.previosStation();
         assertEquals(4, radio.getCurrentStation());
     }
 
     @Test
     public void editCurrentStation(){
-        Radio radio = new Radio("name",100,0,100,5,1,10, 7,true);
-        radio.editcurrentstation();
+        Radio radio = new Radio("name",100,5, true);
+        radio.editcurrentstation(7);
         assertEquals(7,radio.getCurrentStation());
 
     }
     @Test
     public void editCurrentStationMaxLimit() {
-        Radio radio = new Radio("name", 100, 0, 100, 5, 1, 10, 17, true);
-        radio.editcurrentstation();
+        Radio radio = new Radio("name",  100, 5,   true);
+        radio.editcurrentstation(17);
         assertEquals(5, radio.getCurrentStation());
     }
     @Test
     public void editCurrentStationMinLimit() {
-        Radio radio = new Radio("name", 100, 0, 100, 5, 1, 10, 0, true);
-        radio.editcurrentstation();
+        Radio radio = new Radio("name",  100, 5,   true);
+        radio.editcurrentstation(0);
         assertEquals(5, radio.getCurrentStation());
     }
 }
